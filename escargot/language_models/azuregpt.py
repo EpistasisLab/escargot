bfe3eb80bf5e91cda2633c0b9f00da7777440de9
nickotto <ichplaymates@gmail.com>
2024-06-28 18:48:07 -0700

    adding pip config

--- escargot/language_models/azuregpt.py
+++ escargot/language_models/azuregpt.py
@@ -1,11 +1,3 @@
-# Copyright (c) 2023 ETH Zurich.
-#                    All rights reserved.
-#
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# main author: Nils Blach
-
 import backoff
 import os
 import random
--- escargot/operations/operations.py
+++ escargot/operations/operations.py
@@ -1,11 +1,3 @@
-# Copyright (c) 2023 ETH Zurich.
-#                    All rights reserved.
-#
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# main author: Nils Blach
-
 from __future__ import annotations
 import logging
 from enum import Enum
