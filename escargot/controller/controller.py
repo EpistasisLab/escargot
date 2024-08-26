0e259e31bef1ac9a0ccc1524e2559f7ee2ba1656
nickotto <ichplaymates@gmail.com>
2024-08-26 14:40:14 -0700

    copyright

--- escargot/controller/controller.py
+++ escargot/controller/controller.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2023 ETH Zurich.
+#                    All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# main author: Nils Blach
+
 import json
 import logging
 from typing import List, Optional, Dict, Any
--- escargot/language_models/abstract_language_model.py
+++ escargot/language_models/abstract_language_model.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2023 ETH Zurich.
+#                    All rights reserved.
+#
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# main author: Nils Blach
+
 from abc import ABC, abstractmethod
 from typing import List, Dict, Union, Any
 import json
