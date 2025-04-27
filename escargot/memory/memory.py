95fd4b5fa993dffeab3377175fbf7edb7b3cf91e
nickotto <ichplaymates@gmail.com>
2025-04-27 14:50:20 -0700

    raphtory integration for graph database

--- escargot/memory/memory.py
+++ escargot/memory/memory.py
@@ -5,6 +5,9 @@ import networkx as nx
 import uuid
 import pickle
 import os
+from raphtory import Graph
+from raphtory import algorithms as algo
+import shutil
 class Memory:
     def __init__(self, lm, collection_name="default"):
         # Initialize ChromaDB client and specify the collection for storing vectors
@@ -15,9 +18,36 @@ class Memory:
         self.collection_name = collection_name
         self.lm = lm
         self.collection = self.client.get_or_create_collection(collection_name)
+        #if graph exists load it
+        if os.path.exists(f"./escargot_memory/{collection_name}/graph"):
+            self.graph = Graph.load_from_file(f"./escargot_memory/{collection_name}/graph")
+        else:
+            self.graph = Graph()
+            self.save_graph()
 
-    def reset_collection(self):
-        self.client.reset() 
+        #history of when nodes were inserted
+        self.graph_timestamp = 1
+            
+
+    def reset_collection(self, collection_name = None):
+        if collection_name is None:
+            collection_name = self.collection_name
+        
+        self.client.delete_collection(name=collection_name)
+
+        self.client.reset()
+        self.client.clear_system_cache() # very important
+        self.client = None
+
+        if os.path.exists(f"./escargot_memory/{collection_name}"):
+            shutil.rmtree(f"./escargot_memory/{collection_name}")
+        os.makedirs(f"./escargot_memory/{collection_name}")
+
+       # delete you persist_directory and create persist_directory againt
+        self.client = chromadb.PersistentClient(path=f"./escargot_memory/{collection_name}", settings=Settings(allow_reset=True))
+        self.collection = self.client.get_or_create_collection(collection_name)
+        self.graph = Graph()
+        self.save_graph()
 
     def store_memory(self, text, metadata={}, data = None):
         # Embed the text using the lm's embed function
@@ -33,6 +63,13 @@ class Memory:
         # Add the embedded vector to the collection
         collection = self.client.get_collection(self.collection_name)
         collection.add(ids=text, embeddings=[vector], metadatas=metadata)
+
+        self.graph.add_node(
+            timestamp=self.graph_timestamp,
+            id=memory_id,
+            properties={"text":text, "vector":vector},
+        )
+        self.graph_timestamp += 1
 
     def query_collection(self,query, max_results=10, metadata = None):
         collection = self.client.get_collection(self.collection_name)
@@ -80,3 +117,19 @@ class Memory:
         else:
             print(f"Pickle file not found: {file_path}")
             return None
+        
+
+    def save_graph(self, collection_name = None):
+        if collection_name is None:
+            self.graph.save_to_file(f"./escargot_memory/{self.collection_name}/graph")
+        else:
+            if os.path.exists(f"./escargot_memory/{collection_name}/graph"):
+                self.graph.save_to_file(f"./escargot_memory/{collection_name}/graph")
+
+    def load_graph(self, collection_name = None):
+        if collection_name is None:
+            self.graph = Graph.load_from_file(f"./escargot_memory/{self.collection_name}/graph")
+        else:
+            if os.path.exists(f"./escargot_memory/{collection_name}/graph"):
+                self.graph = Graph.load_from_file(f"./escargot_memory/{collection_name}/graph")
+        
